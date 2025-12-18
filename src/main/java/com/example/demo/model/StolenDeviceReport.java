7affcb4aff6c15975b86b37e012204d1dec35748
bala-amypo <bala@amypo.in>
2025-12-18 08:37:49 +0000

    User commit

--- src/main/java/com/example/demo/model/StolenDeviceReport.java
+++ src/main/java/com/example/demo/model/StolenDeviceReport.java
@@ -1,50 +1,57 @@
-// package com.example.demo.model;
-//     public class StolenDeviceReport{
-//         private Long id;
-//         private String serialNumber;
-//         private String reportedBy;
-//         private LocalDateTime reportDate;
-//         private String details;
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
-//         public Long getId(){
-//             return id;
-//         }
-//         public void setId(Long id){
-//             this.id = id;
-//         }
-//         public String getSerialNumber(){
-//             return serialNumber;
-//         }
-//         public void setSerialNumber(String serialNumber){
-//             this.serialNumber = serialNumber;
-//         }
-//         public String getReportedBy(){
-//             return reportedBy
-//         }
-//         public void setReportedBy (String reportedBy){
-//             this.reportedBy = reportedBy;
-//         }
-//         public LocalDateTime getReportDate(){
-//             return reportDate;
-//         }
-//         public void setReportDate(LocalDateTime reportDate){
-//             this.reportDate = reportDate;
-//         }
-//         public String getDetails(){
-//             return details;
-//         }
-//         public void setDetails(String details){
-//             this.details = details;
-//         }
-//         public StolenDeviceReport(Long id,String serialNumber,String reportedBy,LocalDateTime reportDate,String details){
-//             this.id = id;
-//             this.serialNumber = serialNumber;
-//             this.reportedBy = reportedBy;
-//             this.reportDate = reportDate;
-//             this.details = details;
-//         }
-//         public StolenDeviceReport(){
+    public class StolenDeviceReport{
+        private Long id;
+        private String serialNumber;
+        private String reportedBy;
+        private LocalDateTime reportDate;
+        private String details;
+
+        public Long getId(){
+            return id;
+        }
+        public void setId(Long id){
+            this.id = id;
+        }
+        public String getSerialNumber(){
+            return serialNumber;
+        }
+        public void setSerialNumber(String serialNumber){
+            this.serialNumber = serialNumber;
+        }
+        public String getReportedBy(){
+            return reportedBy
+        }
+        public void setReportedBy (String reportedBy){
+            this.reportedBy = reportedBy;
+        }
+        public LocalDateTime getReportDate(){
+            return reportDate;
+        }
+        public void setReportDate(LocalDateTime reportDate){
+            this.reportDate = reportDate;
+        }
+        public String getDetails(){
+            return details;
+        }
+        public void setDetails(String details){
+            this.details = details;
+        }
+        public StolenDeviceReport(Long id,String serialNumber,String reportedBy,LocalDateTime reportDate,String details){
+            this.id = id;
+            this.serialNumber = serialNumber;
+            this.reportedBy = reportedBy;
+            this.reportDate = reportDate;
+            this.details = details;
+        }
+        public StolenDeviceReport(){
             
-//         }
+        }
        
-//     }
+    }
