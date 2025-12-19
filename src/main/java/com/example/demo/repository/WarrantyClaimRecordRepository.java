77c913ee0806442d13f531f5ad5f86117618f5fb
bala-amypo <bala@amypo.in>
2025-12-19 10:30:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
+++ src/main/java/com/example/demo/repository/WarrantyClaimRecordRepository.java
@@ -2,11 +2,10 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.WarrantyClaimRecord;
-import java.util.Optional;
+import java.util.List;
 
 public interface WarrantyClaimRecordRepository extends JpaRepository<WarrantyClaimRecord,Long>{
 
-    boolean existBySerialNumber(String serialNumber);
-    Optional<WarrantyClaimRecord>findBySerialNumber(String serialNumber);
+    List<WarrantyClaimRecord>findBySerialNumber(String serialNumber);
     
 }
