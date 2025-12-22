4cab4442876cc68b018e397008db7694597bfac9
bala-amypo <bala@amypo.in>
2025-12-22 10:17:56 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -7,7 +7,6 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.service.UserService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -16,7 +15,6 @@ import java.util.NoSuchElementException;
 public class UserServiceImpl implements UserService {
 
 private final UserRepository userRepository;
-private final PasswordEncoder passwordEncoder;
 private final JwtTokenProvider jwtTokenProvider;
 
 // REQUIRED constructor (order matters for tests)
