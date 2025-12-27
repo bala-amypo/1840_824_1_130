dbbe967b60a5e3e8d437d0a9a8cb4be3a19b90b4
bala-amypo <bala@amypo.in>
2025-12-27 06:01:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/FraudRuleRepository.java
+++ src/main/java/com/example/demo/repository/FraudRuleRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.model.FraudRule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public interface FraudRuleRepository extends JpaRepository<FraudRule, Long> {
-    List<FraudRule> findByActiveTrue();
+public interface FraudRuleRepository
+        extends JpaRepository<FraudRule, Long> {
+
     FraudRule findByRuleCode(String ruleCode);
+
+    List<FraudRule> findByActiveTrue();
 }
