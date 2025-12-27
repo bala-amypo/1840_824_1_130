edb458a29ca5d131fdf18661cfbe406d33d1c4b4
bala-amypo <bala@amypo.in>
2025-12-27 07:18:57 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,44 +1,66 @@
+// package com.example.demo.dto;
+
+// import java.util.Set;
+
+// public class AuthResponse {
+//     private long id;
+//     private String email;
+//     private Set<String> roles;
+
+//     public AuthResponse() {
+//     }
+
+//     public AuthResponse(long id, String email, Set<String> roles) {
+//         this.id = id;
+//         this.email = email;
+//         this.roles = roles;
+//     }
+
+//     // Getters and setters
+
+//     public long getId() {
+//         return id;
+//     }
+
+//     public void setId(long id) {
+//         this.id = id;
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+
+//     public Set<String> getRoles() {
+//         return roles;
+//     }
+
+//     public void setRoles(Set<String> roles) {
+//         this.roles = roles;
+//     }
+// }
+
 package com.example.demo.dto;
 
-import java.util.Set;
-
 public class AuthResponse {
-    private long id;
-    private String email;
-    private Set<String> roles;
+
+    private String token;
 
     public AuthResponse() {
     }
 
-    public AuthResponse(long id, String email, Set<String> roles) {
-        this.id = id;
-        this.email = email;
-        this.roles = roles;
+    public AuthResponse(String token) {
+        this.token = token;
     }
 
-    // Getters and setters
-
-    public long getId() {
-        return id;
+    public String getToken() {
+        return token;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<String> roles) {
-        this.roles = roles;
+    public void setToken(String token) {
+        this.token = token;
     }
 }
