3416869fd53f668fdd5bcebad0495ca6693c3d0d
bala-amypo <bala@amypo.in>
2025-12-29 10:16:12 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,9 +1,14 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -11,11 +16,34 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        // JWT Security Scheme
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
-                // You need to change the port as per your server
+                .info(new Info()
+                        .title("Warranty Claim Fraud Detection System")
+                        .description(
+                                "Backend REST API for detecting fraudulent warranty claims " +
+                                "using device ownership verification, stolen device tracking, " +
+                                "duplicate claim detection, and rule-based fraud alerts."
+                        )
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Selva Praveen")
+                                .email("tsp@gmail.com")
+                        )
+                )
                 .servers(List.of(
                         new Server().url("https://9044.32procr.amypo.ai/")
                 ));
-        }
+                // Enable Authorize button globally
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .schemaRequirement("bearerAuth", securityScheme);
+    }
 }
+
 
