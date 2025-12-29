efadb13a889e9ac5e8715a69b7886e99346d6080
bala-amypo <bala@amypo.in>
2025-12-29 06:15:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,33 +1,4 @@
-// package com.example.demo.controller;
 
-// import com.example.demo.model.User;
-// import com.example.demo.service.UserService;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/auth")
-// public class AuthController {
-
-//     @Autowired
-//     private UserService userService;
-
-//     @GetMapping("/users")
-//     public List<User> getAllUsers() {
-//         return userService.getAllUsers();
-//     }
-
-//     @GetMapping("/user/{id}")
-//     public User getUser(@PathVariable Long id) {
-//         User u = userService.getUserById(id);
-//         // Fixed getters
-//         System.out.println("Email: " + u.getEmail());
-//         System.out.println("Roles: " + u.getRoles());
-//         return u;
-//     }
-// }
 package com.example.demo.controller;
 
 import com.example.demo.dto.*;
--- src/main/java/com/example/demo/controller/FraudAlertController.java
+++ src/main/java/com/example/demo/controller/FraudAlertController.java
@@ -1,32 +1,3 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.model.FraudAlertRecord;
-// import com.example.demo.service.FraudAlertService;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/alerts")
-// public class FraudAlertController {
-
-//     private final FraudAlertService service;
-
-//     public FraudAlertController(FraudAlertService service) {
-//         this.service = service;
-//     }
-
-//     @GetMapping("/serial/{serial}")
-//     public List<FraudAlertRecord> bySerial(@PathVariable String serial) {
-//         return service.getAlertsBySerial(serial);
-//     }
-
-//     @GetMapping
-//     public List<FraudAlertRecord> all() {
-//         return service.getAllAlerts();
-//     }
-// }
-
 package com.example.demo.controller;
 
 import com.example.demo.model.FraudAlertRecord;
